c7fb728498d2ffdf8ba541df976027cdd8f679bb
liu25 <liuyi19871231@gmail.com>
2016-07-30 18:37:21 -0700

    Update Problem31

--- LeetCode/Problem31.cs
+++ LeetCode/Problem31.cs
@@ -9,8 +9,64 @@ namespace LeetCode
     class Problem31
     {
         public void NextPermutation(int[] nums)
-        {
+	{
+		int length = nums.Length -1;
+		
+    	while(length > 0)
+		{
+			if(nums[length] > nums[length-1])
+				break;
+			length--;
+		}
+		
+		if(length == 0)
+		{
+			int lower = 0;
+			
+			int upper = nums.Length-1;
+			
+			while(lower < upper)
+			{
+				Swap(nums, lower, upper);
+				lower++;
+				upper--;
+			}
+			
+			return;
+		}									
+		
+		Array.Sort(nums, length, nums.Length-length);				
+		
+		FindAndSwap(nums, length-1, length, nums.Length-1);
+	}
 
-        }
+	public void FindAndSwap(int[] nums, int valueIndex, int start, int end)
+	{				
+		if(end == start)
+		{			
+			Swap(nums, start, valueIndex);			
+			return;
+		}
+		
+		int mid = (end + start)/2;
+		
+		if(nums[mid] <= nums[valueIndex])
+		{
+			FindAndSwap(nums, valueIndex, mid+1, end);
+		}
+		else
+		{
+			FindAndSwap(nums, valueIndex, start, mid);
+		}
+	}
+	
+	public void Swap(int[] nums, int index1, int index2)
+	{
+		int temp = nums[index1];
+		
+		nums[index1] = nums[index2];
+		
+		nums[index2] = temp;
+	}
     }
 }
